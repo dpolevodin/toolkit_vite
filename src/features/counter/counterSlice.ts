c7e341842226c72da9d90c1dba9b7937ef243b6d
dpolevodin <dpolevodin@gmail.com>
2023-05-30 17:54:29 +0500

    TEAMCJZPFL-2280 [CRRFMUI]: ЭФ Загруженные файлы (персонификации + активационные): поиск по наименованию

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../../store'
+import { createSlice } from '@reduxjs/toolkit'
 
 export interface CounterState {
     value: number
@@ -27,3 +27,5 @@ export const { increment, decrement } = counterSlice.actions
 export const selectCount = (state: RootState) => state.counter.value
 
 export default counterSlice.reducer
+
+// test
